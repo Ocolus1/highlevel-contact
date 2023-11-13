82767bc1f3ee638cae77e644ae04c53324377071
Paul <awolesiboluwatife@gmail.com>
2023-11-13 14:09:29 +0100

    added google and auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import { AuthProvider } from './context/AuthContext';
+
+
+import Home from "./routes/Home"
+import OnboardingForm from "./routes/OnboardingForm"
+import ErrorPage from "./error-page";
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/onboarding",
+    element: <OnboardingForm />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error('Root element not found');
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <RouterProvider router={router} />
+      </GoogleOAuthProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
