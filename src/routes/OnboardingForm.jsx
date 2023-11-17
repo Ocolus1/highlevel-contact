677373eded3c51c526c3930ad409e6d2d57ea47c
Paul <awolesiboluwatife@gmail.com>
2023-11-17 09:27:18 +0100

    added payment details and auto select next step

--- src/routes/OnboardingForm.jsx
+++ src/routes/OnboardingForm.jsx
@@ -8,11 +8,20 @@ import ScheduleCalendar from '../components/ScheduleCalendar';
 import Navigation from '../components/Navigation';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from "react-router-dom";
+import PaymentDetails from '../components/PaymentDetails';
+import { Alert } from 'react-bootstrap';
+
+import axios from "axios";
+
 
 function MultiStepForm() {
     const [currentStep, setCurrentStep] = useState(1);
-    const { isLoggedIn } = useAuth();
+    const { isLoggedIn, user, getUser, authToken } = useAuth();
     const navigate = useNavigate();
+    const [showFailureAlert, setShowFailureAlert] = useState(false);
+    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
 
     useEffect(() => {
         if (!isLoggedIn) {
@@ -21,12 +30,63 @@ function MultiStepForm() {
         // The cleanup function is optional in this case since navigate is stable
     }, [isLoggedIn, navigate]); 
 
+
+    useEffect(() => {
+        if (user && user?.status) {
+            const stepIndex = steps.findIndex(step => step.label === user?.status);
+            setCurrentStep(stepIndex+1)
+        }
+    }, [user?.status]);
+
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const setupIntentId = urlParams.get('setup_intent');
+        const redirectStatus = urlParams.get('redirect_status');
+
+        if (setupIntentId && redirectStatus === 'succeeded') {
+            // Call your API to link the payment method
+            linkPaymentMethodToCustomer(setupIntentId);
+            navigate("/onboarding")
+            
+        }
+    }, []);
+
+    const linkPaymentMethodToCustomer = async (setupIntentId) => {
+        try {
+
+            const response = await axios.post(`${import.meta.env.VITE_REST_ENDPOINT}/api/link-payment-method/`, 
+                {
+                    "setup_intent_id": setupIntentId
+                },
+                {
+                    headers: {
+                        Authorization: `Token ${authToken}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+            if (response.data.status == "Stripe payment method added successfully.") {
+                setShowSuccessAlert(true)
+                setSuccess("Stripe payment method added successfully.")
+                setTimeout(async () => {
+                    await getUser();
+                }, 2000);
+            }
+        } catch (error) {
+            console.error(error)
+            setShowFailureAlert(true)
+            setError(error.response.data.error)
+        }
+    };
+
     const steps = [
-        { label: "Onboarding", component: <SubAccount setCurrentStep={setCurrentStep} /> },
-        { label: "Phone Purchase", component: <PhoneNumberPurchase setCurrentStep={setCurrentStep} /> },
-        { label: "A2P Registration", component: <A2PRegistration setCurrentStep={setCurrentStep} /> },
-        { label: "Contact Upload", component: <ContactUpload setCurrentStep={setCurrentStep} /> },
-        { label: "Schedule Calendar", component: <ScheduleCalendar setCurrentStep={setCurrentStep} /> },
+        { label: "User Info", component: <SubAccount authToken={authToken} getUser={getUser} /> },
+        { label: "Stripe Payment Details", component: <PaymentDetails authToken={authToken} getUser={getUser} /> },
+        { label: "Phone Number Purchase", component: <PhoneNumberPurchase authToken={authToken} getUser={getUser} /> },
+        { label: "A2P Registration", component: <A2PRegistration authToken={authToken} getUser={getUser} /> },
+        { label: "Contact Upload", component: <ContactUpload authToken={authToken} getUser={getUser} /> },
+        { label: "Schedule Calendar", component: <ScheduleCalendar authToken={authToken} getUser={getUser} /> },
         // ... other steps ...
     ];
 
@@ -41,15 +101,18 @@ function MultiStepForm() {
                 {/* Step Indicator */}
                 <ProgressBar now={progress} label={`${currentStep} / ${steps.length}`} />
 
+                {showFailureAlert && <Alert variant="danger" className="my-2">{error}</Alert>}
+                {showSuccessAlert && <Alert variant="success" className="my-2">{success}</Alert>}
+
                 {/* Render the Current Step */}
                 <div className="mt-4">
                     {steps[currentStep - 1].component}
                 </div>
 
-                <div className="mt-4">
+                {/* <div className="mt-4">
                     {<Button variant="secondary" onClick={() => setCurrentStep(prev => prev - 1)}>Previous</Button>}
                     {<Button className="ml-2" onClick={() => setCurrentStep(prev => prev + 1)}>Next</Button>}
-                </div>
+                </div> */}
             </Container>
         </>
     );
