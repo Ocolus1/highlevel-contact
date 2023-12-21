6affa4ea3800ceb3aef83b5ac0686bfd34c4a23e
Paul <awolesiboluwatife@gmail.com>
2023-12-21 19:54:12 +0100

    added profile page

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+// env.d.ts
+interface ImportMeta {
+    readonly env: {
+        VITE_REST_ENDPOINT: string;
+        // Define other environment variables here if needed
+    };
+}
---
+++ src/routes/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { Dashboard } from './../../../hl-admin/src/Dashboard/index';
+import Navigation from '../components/Navigation';
+import { Container } from 'react-bootstrap';
+
+const UserProfile = () => {
+    const { user } = useAuth();
+
+
+    return (
+        <div>
+            <Navigation />
+            <Container className="mt-5" >
+
+                <h2>Dashboard</h2>
+                {user ? (
+                    <div>
+                        <p>Email: {user?.email}</p>
+                        <p>First Name: {user?.first_name}</p>
+                        <p>Last Name: {user?.last_name}</p>
+                        {/* <p>Business Name: {user.businessName}</p> */}
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </Container>
+        </div>
+    );
+};
+
+export default UserProfile;
